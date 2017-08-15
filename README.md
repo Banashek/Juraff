808b281a314f0d2503c289c7dd356c34da05934d
Jonathan Banashek <jbanashek@gmail.com>
2017-08-14 20:51:03 -0700

    Adding lib and sample

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a minimal copy of the Giraffe library to get things working on my osx laptop with dotnet core 2.0 and aspnet core 2.0
+
+Lots of dependencies axed, and while the full Giraffe library is present, I just wanted to put enough to make a sample application work.
---
+++ samples/HelloWeb/Program.fs
@@ -0,0 +1,70 @@
+﻿module HelloWeb.App
+
+open System
+open System.IO
+open System.Collections.Generic
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Juraff.HttpHandlers
+open Juraff.Middleware
+open Juraff.XmlViewEngine
+
+
+/// Views
+
+
+let layout (content: XmlNode list) =
+    html [] [
+        head [] [
+            title []  [ encodedText "Giraffe" ]
+        ]
+        body [] content
+    ]
+
+let partial () =
+    p [] [ encodedText "Some partial text." ]
+
+let mainView =
+  [ div [] [ partial() ]] |> layout
+
+
+/// App
+
+
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/" >=> (mainView |> renderHtml)
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+
+/// Config
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffeErrorHandler errorHandler
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) = ()       
+
+let configureLogging (loggerBuilder : ILoggingBuilder) =
+    loggerBuilder.AddConsole().AddFilter("System", LogLevel.Debug) |> ignore
+
+[<EntryPoint>]
+let main argv =
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(Action<IServiceCollection> configureServices)
+        .ConfigureLogging(configureLogging)
+        .Build()
+        .Run()
+    0
